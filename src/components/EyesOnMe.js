cf03523b37f8bbedb027126fa0a0ee14d09ed9d4
Rae Stanton <raestanton93@gmail.com>
2023-08-22 13:07:31 -0400

    event listeners

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  const handleFocusEvent = () => {
+    console.log("Good!");
+  };
+
+  const handleBlurEvent = () => {
+    console.log("Hey! Eyes on me!");
+  };
+
+  return (
+    <button className="button" onFocus={handleFocusEvent} onBlur={handleBlurEvent}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,19 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const handlePasswordChange = (e) => {
+    console.log("Entering password...");
+  };
+  return (
+    <div>
+      <input type="password" onChange={handlePasswordChange} />
+    </div>
+  );
 }
 
+// needs input type field to capture input
+// add event handler for input
+// on input add onChange event
+// console log "Entering password..."
 export default Keypad;
